c85b8a51a2f20eaf71a52c1f666f7e5950d5fb17
dumfoundead <69743095+dumfoundead@users.noreply.github.com>
2020-11-16 01:10:10 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Accordion from './state-drills/Accordion';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Accordion />
+      </div>
+    );
+  }
+}
+
+export default App;
